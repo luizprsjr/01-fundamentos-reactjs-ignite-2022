6a9e92a1704b77be08a1185d95a80ab64fc1ec3f
Luiz Pires <lpiresjunior@gmail.com>
2022-08-21 07:44:05 -0300

    post component

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './global.css';
 
 import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Post } from './components/Post';
 import { Sidebar } from './components/Sidebar';
 
 export function App() {
@@ -12,7 +13,14 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-      
+          <Post
+            author="Diego Fernandes"
+            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Maxime itaque quas corporis beatae veritatis, reprehenderit asperiores vitae quod possimus qui dignissimos unde deleniti consequatur quae, repellat debitis sunt, est rerum!"
+          />
+          <Post
+            author="Gabriel Buzzi"
+            content="Um novo post muito legal"
+          />
         </main>
       </div>
     </div>
