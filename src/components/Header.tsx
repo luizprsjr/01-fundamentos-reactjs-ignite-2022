936c74fa1f1f554dca7aab0d270bbdc5564f250c
Luiz Pires <lpiresjunior@gmail.com>
2022-08-21 06:51:15 -0300

    header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,10 @@
+import igniteLogo from '../assets/ignite-logo.svg';
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <strong>Ignite Feed</strong>
+      <img src={igniteLogo} alt="Logotipo do Ignite" />
     </header>
   );
 }
