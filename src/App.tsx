4c5b22d1b791a63ddfbb1c359aa2c60eb94c6f8d
Luiz Pires <lpiresjunior@gmail.com>
2022-08-21 07:25:40 -0300

    sidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
 import './global.css';
 
+import styles from './App.module.css';
 import { Header } from './components/Header';
+import { Sidebar } from './components/Sidebar';
 
 export function App() {
   return (
     <div>
       <Header />
+
+      <div className={styles.wrapper}>
+        <Sidebar />
+        <main>
+      
+        </main>
+      </div>
     </div>
   )
 }
