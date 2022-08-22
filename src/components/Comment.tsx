dc8651c3b876ae7f78a207edc6e052ee276d3c37
Luiz Pires <lpiresjunior@gmail.com>
2022-08-22 14:37:25 -0300

    comment like

--- src/components/Comment.tsx
+++ src/components/Comment.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react';
+
 import { ThumbsUp, Trash } from 'phosphor-react';
 
 import { Avatar } from './Avatar';
@@ -9,9 +11,16 @@ interface CommentProps {
 }
 
 export function Comment({ content, onDeleteComment }: CommentProps) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content);
+  }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1
+    });
   }
 
   return (
@@ -39,9 +48,9 @@ export function Comment({ content, onDeleteComment }: CommentProps) {
         </div>
 
         <footer>
-          <button>
+          <button onClick={handleLikeComment}>
             <ThumbsUp />
-            Aplaudir <span>20</span>
+            Aplaudir <span>{likeCount}</span>
           </button>
         </footer>
       </div>
